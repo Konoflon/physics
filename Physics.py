979712957fe017a3dd4460cceac7347dbdc6a5d8
Konoflon <meliakov_iv@edu.surgu.ru>
2026-05-12 18:48:21 +0500

    physics

---
+++ Physics.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+angle = float(input("Угол (град): "))
+mu = float(input("Коэфф трения: "))
+g = 9.81
+a = g * (np.sin(np.radians(angle)) - mu * np.cos(np.radians(angle)))
+if a < 0: a = 0
+
+dt = 0.1
+t = np.arange(0, 10, dt)
+v = np.zeros(len(t))
+acc = np.full(len(t), a)
+
+for i in range(1, len(t)):
+    v[i] = v[i-1] + a * dt
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+ax1.plot(t, v)
+ax2.plot(t, acc, color='r')
+ax1.set_ylabel('Скорость (м/с)')
+ax2.set_ylabel('Ускорение (м/с^2)')
+ax2.set_xlabel('Время (с)')
+plt.show()
